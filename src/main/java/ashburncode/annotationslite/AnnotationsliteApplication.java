4b9f96ddf775a8b4e14e78f38ff34500a1d3f40e
ashburndev <ashburndev@gmail.com>
2024-07-14 18:43:35 -0400

    initial commit

---
+++ src/main/java/ashburncode/annotationslite/AnnotationsliteApplication.java
@@ -0,0 +1,13 @@
+package ashburncode.annotationslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationsliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationsliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/annotationslite/AnnotationsliteApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.annotationslite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationsliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
